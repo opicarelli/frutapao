1e3f12a43940410ca08cf1721fd81c4f2e06e2ca
Marcos Oto Picarelli Prado <marcosp@cpqd.com.br>
2018-06-19 16:44:30 -0300

    Incluindo datatables - exemplo na tela de estoque

---
+++ src/main/java/com/opicarelli/frutapao/controller/EstoqueController.java
@@ -0,0 +1,15 @@
+package com.opicarelli.frutapao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class EstoqueController {
+
+	@RequestMapping(value = "/estoque", method = RequestMethod.GET)
+	public String estoque() {
+		return "estoque";
+	}
+
+}
